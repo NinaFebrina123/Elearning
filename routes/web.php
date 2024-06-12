800bb5de6436d36fe568fa45f1e1e725c15ae664
Nina Febrina Siregar <ninafebrinasiregar03@gmail.com>
2024-06-12 15:48:41 +0700

    praktikum 13

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,24 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CoursesController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StudentController;
-use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified']);
+
+Route::middleware('auth')->group(function () {
+    
+Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
 // route untuk menampilkan halaman student
 Route::get('admin/student', [StudentController::class, 'index']);
 
@@ -46,3 +54,10 @@ Route::put('admin/courses/update/{id}', [CoursesController::class, 'update']);
 
 // route untuk menghapus courses
 Route::delete('admin/courses/delete/{id}', [CoursesController::class, 'destroy']);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
